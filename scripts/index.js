e77cb38d4a57b631d39dd377ae723ba91f55b064
anngri411 <161143122+anngri411@users.noreply.github.com>
2024-09-24 14:10:42 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(card, callBackDeleteCard) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => {
+        callBackDeleteCard(cardElement);
+    });
+    return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+    const readyElement = createCard(card, deleteCard);
+    placesList.append(readyElement);
+});
