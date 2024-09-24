73ce9bbaf33a8814496c671ccbd42f73d7096f72
anngri411 <161143122+anngri411@users.noreply.github.com>
2024-09-24 16:55:54 +0300

    Update index.js
    fix alt

--- scripts/index.js
+++ scripts/index.js
@@ -2,28 +2,30 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(card, callBackDeleteCard) {
+function createCard(cardData, callBackDeleteCard) {
     const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
+    cardElement.querySelector('.card__title').textContent = cardData.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', () => {
         callBackDeleteCard(cardElement);
     });
+
     return cardElement;
 };
 
 // @todo: Функция удаления карточки
-function deleteCard(card) {
-    card.remove();
+function deleteCard(cardData) {
+    cardData.remove();
 };
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function(card) {
-    const readyElement = createCard(card, deleteCard);
-    placesList.append(readyElement);
+initialCards.forEach(function(cardData) {
+    const readyElement = createCard(cardData, deleteCard);
+    cardsContainer.append(readyElement);
 });
